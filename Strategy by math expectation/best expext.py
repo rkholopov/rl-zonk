a1343a78887eec9cbf1c3fd7744fdf617b47ded4
Роман Холопов <r.kholopov@gmail.com>
2026-06-03 20:51:06 +0300

    Доделал функцию reward

--- Strategy by math expectation/best expext.py
+++ Strategy by math expectation/best expext.py
@@ -4,6 +4,14 @@ from functools import lru_cache
 def reward(x, k):
     if k==6 and x>=16450:
         return x
+    if k==5 and x>=3050:
+        return x
+    if k==4 and x>=1050:
+        return x
+    if k==3 and x>=400:
+        return x
+    if k<3 and x>=300:
+        return x
     m = 0
     for i1 in range(1, 7):
         for i2 in range(1, 7):
@@ -13,70 +21,72 @@ def reward(x, k):
                         for i6 in range(1, 7):
                             best = 0
                             c = sorted([i1, i2+9*(k<2), i3+9*(k<3), i4+9*(k<4), i5+9*(k<5), i6+9*(k<6)])
-                            print(c)
-                            if c[0] == c[1] - 1 == c[2] - 2 == c[3] - 3 == c[4] - 4 == c[5] - 5 and k==6:
-                                best = reward(x+1500,6)
-                            if c[0] == c[1] and c[2] == c[3] and c[4] == c[5] and k==6:
-                                best = max(best, reward(x+750,6))
-                            if c[0] == c[1] == c[2] and c[3] == c[4] == c[5] and k==6:
-                                if c[0] == 1:
-                                    best = max(best, reward(x+c[0]*1000+c[3]*100,6))
-                                else:
-                                    best = max(best, reward(x+c[0]*100+c[3]*100,6))
 
-                            if c.count(1) >= 3:
-                                y = 1000 * (c.count(1) - 2) + 50 * c.count(5)
-                                if c.count(5) + c.count(1) == k and x + y < 16450:
-                                    best = max(best, reward(x+y, 6))
-                                else:
-                                    best = max(best, x+y)
+                            if c[-1]<=10:
+                                if c[0] == c[1] - 1 == c[2] - 2 == c[3] - 3 == c[4] - 4 == c[5] - 5 and k==6:
+                                    best = reward(x+1500,6)
+                                if c[0] == c[1] and c[2] == c[3] and c[4] == c[5] and k==6:
+                                    best = max(best, reward(x+750,6))
+                                if c[0] == c[1] == c[2] and c[3] == c[4] == c[5] and k==6:
+                                    if c[0] == 1:
+                                        best = max(best, reward(x+c[0]*1000+c[3]*100,6))
+                                    else:
+                                        best = max(best, reward(x+c[0]*100+c[3]*100,6))
 
-                            if c.count(2) >= 3:
-                                y = 200 * (c.count(2) - 2) + 50 * c.count(5) + 100 * c.count(1)
-                                if c.count(5) + c.count(1) + c.count(2) == k and x + y < 16450:
-                                    best = max(best, reward(x+y, 6))
-                                else:
-                                    best = max(best, x + y)
+                                if c.count(1) >= 3:
+                                    y = 1000 * (c.count(1) - 2) + 50 * c.count(5)
+                                    if c.count(5) + c.count(1) == k:
+                                        best = max(best, reward(x+y, 6))
+                                    else:
+                                        best = max(best, reward(x+y, k-c.count(1)-c.count(5)))
 
-                            if c.count(3) >= 3:
-                                y = 300 * (c.count(3) - 2) + 50 * c.count(5) + 100 * c.count(1)
-                                if c.count(5) + c.count(1) + c.count(3) == k and x + y < 16450:
-                                    best = max(best, reward(x+y, 6))
-                                else:
-                                    best = max(best, x + y)
+                                if c.count(2) >= 3:
+                                    y = 200 * (c.count(2) - 2) + 50 * c.count(5) + 100 * c.count(1)
+                                    if c.count(5) + c.count(1) + c.count(2) == k:
+                                        best = max(best, reward(x+y, 6))
+                                    else:
+                                        best = max(best, reward(x+y, k-c.count(1)-c.count(5)-c.count(2)))
 
-                            if c.count(4) >= 3:
-                                y = 400 * (c.count(4) - 2) + 50 * c.count(5) + 100 * c.count(1)
-                                if c.count(5) + c.count(1) + c.count(4) == k and x + y < 16450:
-                                    best = max(best, reward(x+y, 6))
-                                else:
-                                    best = max(best, x + y)
+                                if c.count(3) >= 3:
+                                    y = 300 * (c.count(3) - 2) + 50 * c.count(5) + 100 * c.count(1)
+                                    if c.count(5) + c.count(1) + c.count(3) == k and x + y < 16450:
+                                        best = max(best, reward(x+y, 6))
+                                    else:
+                                        best = max(best, reward(x+y, k-c.count(1)-c.count(5)-c.count(3)))
 
-                            if c.count(5) >= 3:
-                                y = 500 * (c.count(5) - 2) + 100 * c.count(1)
-                                if c.count(5) + c.count(1) == k and x + y < 16450:
-                                    best = max(best, reward(x+y, 6))
-                                else:
-                                    best = max(best, x + y)
+                                if c.count(4) >= 3:
+                                    y = 400 * (c.count(4) - 2) + 50 * c.count(5) + 100 * c.count(1)
+                                    if c.count(5) + c.count(1) + c.count(4) == k and x + y < 16450:
+                                        best = max(best, reward(x+y, 6))
+                                    else:
+                                        best = max(best, reward(x+y, k-c.count(1)-c.count(5)-c.count(4)))
 
-                            if c.count(6) >= 3:
-                                y = 600 * (c.count(6) - 2) + 50 * c.count(5) + 100 * c.count(1)
-                                if c.count(6) + c.count(1) + c.count(5) == k and x + y < 16450:
-                                    best = max(best, reward(x+y, 6))
-                                else:
-                                    best = max(best, x + y)
+                                if c.count(5) >= 3:
+                                    y = 500 * (c.count(5) - 2) + 100 * c.count(1)
+                                    if c.count(5) + c.count(1) == k and x + y < 16450:
+                                        best = max(best, reward(x+y, 6))
+                                    else:
+                                        best = max(best, reward(x+y, k-c.count(1)-c.count(5)))
 
-                            y = 50 * c.count(5) + 100 * c.count(1)
-                            if c.count(1) + c.count(5) == k and x + y < 16450 and k<=4:
-                                best = max(best, reward(x + y, 6))
-                            else:
-                                best = max(best, x + y)
+                                if c.count(6) >= 3:
+                                    y = 600 * (c.count(6) - 2) + 50 * c.count(5) + 100 * c.count(1)
+                                    if c.count(6) + c.count(1) + c.count(5) == k and x + y < 16450:
+                                        best = max(best, reward(x+y, 6))
+                                    else:
+                                        best = max(best, reward(x+y, k-c.count(1)-c.count(5)-c.count(6)))
 
-                            if best != 0:
-                                m += best / 6 ** 6
+                                y = 50 * c.count(5) + 100 * c.count(1)
+                                if c.count(1) + c.count(5) == k and k<=4:
+                                    best = max(best, reward(x + y, 6))
+                                elif y>0:
+                                    best = max(best, reward(x+y, k-c.count(1)-c.count(5)))
+
+                                if best != x:
+                                    m += best / 6 ** k
     return m
 
-start = 30000
-r = reward(start, 5)
+
+start = 0
+r = reward(start, 6)
 
 print(start, r-start)
