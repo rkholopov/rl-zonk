07da899e4cfdf9cb5517ffe949b91f75bccf000a
Роман Холопов <r.kholopov@gmail.com>
2026-06-10 17:08:34 +0300

    Сделал вероятность вместо кол-ва и исправил баги

--- Strategy by math expectation/multiple_distribution.py
+++ Strategy by math expectation/multiple_distribution.py
@@ -4,12 +4,14 @@ from openpyxl import load_workbook
 
 r, dist = reward(0, 6, show_distribution=True)
 
-compact_dist = [[dist[0], 1]]
+compact_dist = [[int(dist[0]), 1 / 6**6]]
 for i in range(1, len(dist)):
-    if dist[i] != compact_dist[-1][0]:
-        compact_dist.append([dist[i], 1])
+    if int(dist[i]) != compact_dist[-1][0]:
+        compact_dist.append([int(dist[i]), 1 / 6**6])
     else:
-        compact_dist[-1][1] += 1
+        compact_dist[-1][1] += 1 / 6**6
+
+print(compact_dist)
 
 mult_dist = deepcopy(compact_dist)
 for k in range(2):
@@ -17,12 +19,12 @@ for k in range(2):
     dict_next_mult_dist = dict()
     for i in range(len(mult_dist)):
         for j in range(len(compact_dist)):
-            x = mult_dist[i][0] + compact_dist[j][0]
+            x = int(mult_dist[i][0] + compact_dist[j][0])
             cnt = mult_dist[i][1] * compact_dist[j][1]
-            if tuple([x, cnt]) in dict_next_mult_dist.keys():
-                next_mult_dist[dict_next_mult_dist[tuple([x, cnt])]][1] += cnt
+            if x in dict_next_mult_dist.keys():
+                next_mult_dist[dict_next_mult_dist[x]][1] += cnt
             else:
-                dict_next_mult_dist[tuple([x, cnt])] = len(next_mult_dist)
+                dict_next_mult_dist[x] = len(next_mult_dist)
                 next_mult_dist.append([x, cnt])
 
     mult_dist = deepcopy(next_mult_dist)
