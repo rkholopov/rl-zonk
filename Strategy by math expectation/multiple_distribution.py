5440ff31b42bdc2f4042a1db6cc777cb5080eef7
Роман Холопов <r.kholopov@gmail.com>
2026-06-11 19:56:54 +0300

    Доработал вывод данных в Excel

--- Strategy by math expectation/multiple_distribution.py
+++ Strategy by math expectation/multiple_distribution.py
@@ -1,25 +1,24 @@
-from best_expext import reward
 from copy import deepcopy
 from openpyxl import load_workbook
+import pandas as pd
 
-r, dist = reward(0, 6, show_distribution=True)
+dist = pd.read_excel('Distribution_of_SBME.xlsx', sheet_name='Sheet1')
+dist = dist[0].tolist()
 
-compact_dist = [[int(dist[0]), 1 / 6**6]]
+compact_dist = [[int(dist[0]) - int(dist[0]) % 50, 1 / 6**6]]
 for i in range(1, len(dist)):
-    if int(dist[i]) != compact_dist[-1][0]:
-        compact_dist.append([int(dist[i]), 1 / 6**6])
+    if int(dist[i]) - int(dist[i]) % 50 != compact_dist[-1][0]:
+        compact_dist.append([int(dist[i]) - int(dist[i]) % 50, 1 / 6**6])
     else:
         compact_dist[-1][1] += 1 / 6**6
 
-print(compact_dist)
-
 mult_dist = deepcopy(compact_dist)
-for k in range(2):
+for k in range(9):
     next_mult_dist = []
     dict_next_mult_dist = dict()
     for i in range(len(mult_dist)):
         for j in range(len(compact_dist)):
-            x = int(mult_dist[i][0] + compact_dist[j][0])
+            x = int(mult_dist[i][0] + compact_dist[j][0]) - int(mult_dist[i][0] + compact_dist[j][0]) % 50
             cnt = mult_dist[i][1] * compact_dist[j][1]
             if x in dict_next_mult_dist.keys():
                 next_mult_dist[dict_next_mult_dist[x]][1] += cnt
@@ -29,10 +28,11 @@ for k in range(2):
 
     mult_dist = deepcopy(next_mult_dist)
 
-
+'''
 wb = load_workbook("Distribution_of_SBME.xlsx")
-ws = wb.create_sheet("dist_3_rounds")
+ws = wb.create_sheet("dist_10_rounds")
 for row in mult_dist:
     ws.append(row)
 
 wb.save("Distribution_of_SBME.xlsx")
+'''
